2d4693a9917f72048f108d1e94addd132a1a9c3d
Kcondeg <97910684+Kcondeg@users.noreply.github.com>
2025-03-19 18:56:18 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,19 +7,6 @@ df = pd.read_excel('SalidaFinalVentas.xlsx')
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-
-# Reemplaza 'SalidaFinalVentas.xlsx' con la ruta correcta a tu archivo
-df = pd.read_excel('SalidaFinalVentas.xlsx')
-
-# Muestra las primeras filas del DataFrame para verificar que se cargó correctamente
-print(df.head()) 
-
-# Reemplaza 'SalidaFinalVentas.xlsx' con la ruta correcta a tu archivo
-df = pd.read_excel('SalidaFinalVentas.xlsx')
-
-# Muestra el DataFrame usando Streamlit
-st.dataframe(df) 
-
 # Agrupar las ventas por región y sumarlas
 ventas_por_region = df.groupby('Region')['Ventas'].sum()
 
